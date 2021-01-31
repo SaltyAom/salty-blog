deca591e7b29e85ce928b02839eedec71b710423
SaltyAom <aomkirby123@gmail.com>
2021-01-31 23:12:27 +0700

    :wrench: fix - landing banner dark mode

--- src/layouts/landing/banner/index.tsx
+++ src/layouts/landing/banner/index.tsx
@@ -7,23 +7,28 @@ const Banner = () => (
         <div
             className={`${styles.banner} flex flex-col-reverse sm:flex-nowrap sm:flex-row justify-between items-center px-8 pt-0 sm:pt-12 pb-6 mx-auto`}
         >
-            <section className={`${styles['banner-content']} mt-8 sm:mt-0 flex flex-col`}>
-                <h1 className="text-4xl m-0 mb-4 sm:mb-1 font-medium leading-snug">
+            <section
+                className={`${styles['banner-content']} mt-8 sm:mt-0 flex flex-col`}
+            >
+                <h1 className="text-4xl text-gray-700 dark:text-gray-100 m-0 mb-4 sm:mb-1 font-medium leading-snug">
                     Blog ของ Dev ที่ชอบ
                     <mark
-                        className="bg-transparent border-0 border-b-4 border-solid border-blue-400"
+                        className="bg-transparent border-0 border-b-4 border-solid color-current border-blue-400"
                         title="Nendoroid หรือคนไทยเรียกสั้นๆ ว่า เนนด๋อย"
+                        style={{
+                            color: 'currentcolor'
+                        }}
                     >
                         ด๋อย
                     </mark>
                 </h1>
-                <p className="my-4 text-xl font-light text-gray-400 leading-8 whitespace-pre-wrap">
+                <p className="my-4 text-xl font-light text-gray-400 dark:text-gray-400 leading-8 whitespace-pre-wrap">
                     สรุปเนื้อหาเรื่อง Programming ไว้อ่านเอง
                     <br />
                     แต่ทำให้อ่านง่ายเผื่อตัวเองลืม
                 </p>
                 <ul
-                    className={`${styles['banner-list']} my-0 pl-1 list-none font-light text-xl text-gray-400 leading-8`}
+                    className={`${styles['banner-list']} my-0 pl-1 list-none font-light text-xl text-gray-400 dark:text-gray-400 leading-8`}
                 >
                     <li>Web Development</li>
                     <li>React, TypeScript, Flutter</li>
